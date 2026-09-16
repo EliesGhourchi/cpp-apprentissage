f7c9ef48a2187f6641cbf7cb601bf4cfa3823698
Elies Ghourchi <ghourchi.elies63@gmail.com>
2026-09-16 23:34:41 +0200

    End Exo1 Read and return String and int

--- 01-bases/main.cpp
+++ 01-bases/main.cpp
@@ -4,20 +4,41 @@
 
 int main() {
     std::string playerName;
-    std::cout << "UserName : " ;
+    std::cout << "UserName: " ;
     std::getline(std::cin, playerName);
     
     int playerLevel = 0;
-    std::cout << "Level :";
-    std::cin >> playerLevel;
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    while(playerLevel <= 0 || playerLevel > 20)
+    {
+        std::cout << "Level:";
+        std::cin >> playerLevel;
 
+        if(std::cin.fail())
+        {
+            std::cin.clear();
+            std::cout << "Not a number\n";
+        }
+        else if(playerLevel < 0)
+        {
+            std::cout << "Negative value not accepted\n";
+        }
+        else if(playerLevel > 20)
+        {
+            std::cout << "Max level is 20\n";
+        }
+        else if(playerLevel == 0)
+        {
+            std::cout << "Level 0 not accepted\n";
+        }
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    
     std::string playerClass;
-    std::cout << "Class : ";
+    std::cout << "Class: ";
     std::getline(std::cin, playerClass);
 
-    std::cout << "Welcome: " << playerName << ", your class is: " << playerClass 
-    << ", level: " << playerLevel << "\n";
+    std::cout << "Welcome: " << playerName << ", your class is: " << playerClass << ", level: " << playerLevel << "\n";
 
     return 0;
 }
