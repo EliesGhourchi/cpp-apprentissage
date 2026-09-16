9e1077c846b6bf9653954397de61e081c737e34c
Elies Ghourchi <ghourchi.elies63@gmail.com>
2026-09-16 22:21:25 +0200

    Save Step4 Read and Write

--- 01-bases/main.cpp
+++ 01-bases/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <limits>
 
 int main() {
-    std::cout << "bases" << std::endl;
+    std::string playerName;
+    std::cout << "UserName : " ;
+    std::getline(std::cin, playerName);
+    
+    int playerLevel = 0;
+    std::cout << "Level :";
+    std::cin >> playerLevel;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::string playerClass;
+    std::cout << "Class : ";
+    std::getline(std::cin, playerClass);
+
+    std::cout << "Welcome: " << playerName << ", your class is: " << playerClass 
+    << ", level: " << playerLevel << "\n";
+
     return 0;
 }
